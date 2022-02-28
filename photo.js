c6b96d386c8297d5b56d7c59fb3f169d91c21e5d
adazakuraa <99201782+adazakuraa@users.noreply.github.com>
2022-03-01 00:07:28 +0900

    Add files via upload

---
+++ photo.js
@@ -0,0 +1,169 @@
+let pic_bird=["inndokuzixyaku（kizi科）","ukoxtukei（kizi科）","uzura（kizi科）","ezoraitixyou（kizi科）","kannmuriseirann（kizi科）","kizi（kizi科）","kouraikizi（kizi科）","kozixyukei（kizi科）","sitimenntixyou（kizi科）","sixyamo（kizi科）","tixyabo（kizi科）","nizikizi（kizi科）","hinaidori（kizi科）","benizixyukei（kizi科）","yamadori（kizi科）","yoーroxtupaooraitixyou（kizi科）","raitixyou（kizi科）","oohoukanntixyou（houkanntixyou科）","husahorohorotixyou（horohorotixyou科）","horohorotixyou（horohorotixyou科）","yabutukatukuri（tukatukuri科）","yosigamo（kamo科）","aigamo（kamo科）","akatukusigamo（kamo科）","ahiru（kamo科）","amerikahidori（kamo科）","oohakutixyou（kamo科）","okayosigamo（kamo科）","osidori（kamo科）","onagakamo（kamo科）","kaozirogann（kamo科）","kasasagigann（kamo科）","karigane（kamo科）","karugamo（kamo科）","gatixyou（kamo科）","kinnkurohaziro（kamo科）","kurogamo（kamo科）","koorigamo（kamo科）","kogamo（kamo科）","kokugann（kamo科）","kokutixyou（kamo科）","kohakutixyou（kamo科）","sakaturagann（kamo科）","sizixyuukaragann（kamo科）","sinagatixyou（kamo科）","sinorigamo（kamo科）","simaazi（kamo科）","suzugamo（kamo科）","tukusigamo（kamo科）","tomoegamo（kamo科）","nakihakutixyou（kamo科）","nioigamo（kamo科）","nobarikenn（kamo科）","haiirogann（kamo科）","hakugann（kamo科）","hasibirogamo（kamo科）","hisikui（kamo科）","biroーdokinnkuro（kamo科）","hoozirogamo（kamo科）","hosihaziro（kamo科）","magamo（kamo科）","magann（kamo科）","meganekewatagamo（kamo科）","kannmurisakebidori（sakebidori科）","kuginagakaituburi（kaituburi科）","kaituburi（kaituburi科）",
+"kannmurikaituburi（kaituburi科）","siraonextutaitixyou（nextutaitixyou科）","akaonextutaitixyou（nextutaitixyou科）","sakei（sakei科）","sirakobato（hato科）","aobato（hato科）","ougibato（hato科）","karasubato（hato科）","kizibato（hato科）","kinnbato（hato科）","minobato（hato科）","moribato（hato科）","abi（abi科）","siroerioohamu（abi科）","hasiziroabi（abi科）","oomizunagidori（mizunagidori科）","oohurumakamome（mizunagidori科）","hurumakamome（mizunagidori科）","watariahoudori（ahoudori科）","ahoudori（ahoudori科）","kuroasiahoudori（ahoudori科）","mayuguroahoudori（ahoudori科）","asinagaumitubame（umitubame科）","kosiziroumitubame（umitubame科）","haiiroumitubame（umitubame科）","inndotokikou（kounotori科）","kurahasikou（kounotori科）","kounotori（kounotori科）","nabekou（kounotori科）","aoturakatuodori（katuodori科）","umiu（u科）","kawau（u科）","tisimaugarasu（u科）","himeu（u科）","momoiroperikann（perikann科）","kaxtusixyokuperikann（perikann科）","oogunnkanndori（gunnkanndori科）","zuguromizogoi（sagi科）","aosagi（sagi科）","akagasirasagi（sagi科）","amasagi（sagi科）","karasirasagi（sagi科）","kurosagi（sagi科）","kosagi（sagi科）","goisagi（sagi科）","sannkanogoi（sagi科）","daisagi（sagi科）","herasagi（sagi科）","murasakisagi（sagi科）","yosigoi（sagi科）","sixyumokudori（sixyumokudori科）","hasibirokou（hasibirokou科）","aziahebiu（hebiu科）","toki（toki科）","kurotoki（toki科）","sixyouzixyoutoki（toki科）","ooduru（turu科）","sodeguroduru（turu科）","tanntixyou（turu科）","turumodoki（turu科）","nabeduru（turu科）","hoozirokannmuriduru（turu科）",
+"manaduru（turu科）","amerikahireasi（hireasi科）","yannbarukuina（kuina科）","oobann（kuina科）","kuina（kuina科）","takahe（kuina科）","bann（kuina科）","hikuina（kuina科）","kaxtukou（kaxtukou科）","zixyuuiti（kaxtukou科）","hototogisu（kaxtukou科）","risukaxtukou（kaxtukou科）","yotaka（yotaka科）","rakextutoyotaka（yotaka科）","amatubame（amatubame科）","amatubame（amatubame科）","kosirahigekannmuriamatubame（kannmuriamatubame科）","zugurotosakageri（tidori科）","ikarutidori（tidori科）","keri（tidori科）","tageri（tidori科）","munaguro（tidori科）","ahurikahasamiazisasi（kamome科）","umineko（kamome科）","eriguroazisasi（kamome科）","ooazisasi（kamome科）","kamome（kamome科）","kixyokuazisasi（kamome科）","kuroazisasi（kamome科）","segurokamome（kamome科）","zugurokamome（kamome科）","beniazisasi（kamome科）","yurikamome（kamome科）","mihuuzura（mihuuzura科）","miyakodori（miyakodori科）","rennkaku（rennkaku科）","sayahasitidori（sayahasitidori科）","sorihasiseitakasigi（seitakasigi科）","seitakasigi（seitakasigi科）","yamasigi（sigi科）","aoasisigi（sigi科）","erimakisigi（sigi科）","ogurosigi（sigi科）","kixyouzixyosigi（sigi科）","herasigi（sigi科）","tamasigi（tamasigi科）","tokihasigeri（tokihasigeri科）","touzokukamome（touzokukamome科）","tubametidori（tubametidori科）","utou（umisuzume科）","umioumu（umisuzume科）","umigarasu（umisuzume科）","umisuzume（umisuzume科）","umibato（umisuzume科）","etopirika（umisuzume科）","sirahigeumisuzume（umisuzume科）","tunomedori（umisuzume科）","tobi（taka科）","akaharadaka（taka科）","inuwasi（taka科）","eziputohagewasi（taka科）","ootaka（taka科）",
+"oowasi（taka科）","ozirowasi（taka科）","kannmuriwasi（taka科）","kumataka（taka科）","darumawasi（taka科）","tixyuuhi（taka科）","nosuri（taka科）","hakutouwasi（taka科）","hatikuma（taka科）","higewasi（taka科）","madagasukarutixyuuhidaka（taka科）","hebikuiwasi（hebikuiwasi科）","konndoru（konndoru科）","kotodori（kotodori科）","misago（misago科）","wasimimizuku（hukurou科）","aobazuku（hukurou科）","ookonohazuku（hukurou科）","kinnmehukurou（hukurou科）","konohazuku（hukurou科）","simahukurou（hukurou科）","sirohukurou（hukurou科）","torahuzuku（hukurou科）","mennhukurou（hukurou科）","saitixyou（saitixyou科）","yatugasira（yatugasira科）","buxtupousou（buxtupousou科）","benihatikui（hatikui科）","yamasixyoubinn（kawasemi科）","akasixyoubinn（kawasemi科）","ooonagasixyouginn（kawasemi科）","kawasemi（kawasemi科）","nannyousixyoubinn（kawasemi科）","yamasemi（kawasemi科）","oobuxtupousou（oobuxtupousou科）","nogutigera（kitutuki科）","aogera（kitutuki科）","akagera（kitutuki科）","arisui（kitutuki科）","kumagera（kitutuki科）","kogera（kitutuki科）","nodoguromituosie（mituosie科）","sannsixyokukimuneoohasi（oohasi科）","tixyougennbou（hayabusa科）","sirohayabusa（hayabusa科）","hayabusa（hayabusa科）","suzume（suzume科）","garaパgosuhuxinnti（huukinntixyou科）","simanoziko（hooziro科）","aozi（hooziro科）","aozi（hooziro科）","kurozi（hooziro科）","kurozi（hooziro科）","kozixyurinn（hooziro科）","kozixyurinn（hooziro科）","simaaozi（hooziro科）","simaaozi（hooziro科）","simanoziko（hooziro科）","hooziro（hooziro科）","hooziro（hooziro科）","miyamahooziro（hooziro科）",
+"miyamahooziro（hooziro科）","kannzasihuutixyou（huutixyou科）","aohuutixyou（huutixyou科）","hukinagasihuutixyou（huutixyou科）","iwahibari（iwahibari科）","kabebasiri（kabebasiri科）","zixyuusimatu（kaedetixyou科）","kokinntixyou（kaedetixyou科）","bunntixyou（kaedetixyou科）","benisuzume（kaedetixyou科）","onaga（karasu科）","kakesu（karasu科）","kasasagi（karasu科）","kokumarugarasu（karasu科）","zuguromorimozu（karasu科）","hasibutogarasu（karasu科）","hosigarasu（karasu科）","miyamagarasu（karasu科）","rurikakesu（karasu科）","watarigarasu（karasu科）","sannkoutixyou（kasasagihitaki科）","kawagarasu（kawagarasu科）","hibari（hibari科）","higedori（kazaridori科）","gianaiwadori（kazaridori科）","sirogasira（hiyodori科）","kibasiri（kibasiri科）","hiyodori（hiyodori科）","kikuitadaki（kikuitadaki科）","onagaseaomaikodori（maikodori科）","kouraiuguisu（kouraiuguisu科）","meganekouraiuguisu（kouraiuguisu科）","aomenisetaiyoutixyou（mamiyairotixyou科）","misosazai（misosazai科）","meguro（meziro科）","meziro（meziro科）","gozixyuukara（gozixyuukara科）","zuakamitusui（mitusui科）","tigomozu（mozu科）","ookaramozu（mozu科）","mozu（mozu科）","zixyougitaki（hitaki科）","akakoxtuko（hitaki科）","akahige（hitaki科）","isohiyodori（hitaki科）","ooruri（hitaki科）","ogawakomadori（hitaki科）","karaakahara（hitaki科）","kigitaki（hitaki科）","komadori（hitaki科）","koruri（hitaki科）","tugumi（hitaki科）","toratugumi（hitaki科）","rurigitaki（hitaki科）","nisimakibadori（mukudorimodoki科）","oomusikui（musikui科）","akahasiusitutuki（mukudori科）",
+"tukinowaterimuku（mukudori科）","mukudori（mukudori科）","uso（atori科）","atori（atori科）","ikaru（atori科）","isuka（atori科）","oomasiko（atori科）","kanaria（atori科）","benihiwa（atori科）","mahiwa（atori科）","benihawaimitusui（hawaimitusui科）","nekodori（niwasidori科）","herumextutooohasimozu（oohasimozu科）","enaga（enaga科）","sixyakaihataoridori（hataoridori科）","kouyoutixyou（hataoridori科）","kirennzixyaku（rennzixyaku科）","hukinagasihatidori（hatidori科）","mamehatidori（hatidori科）","yarihasihatidori（hatidori科）","sannsixyoukui（sannsixyoukui科）","oosextuka（sennnixyuu科）","kisekirei（sekirei科）","tahibari（sekirei科）","hakusekirei（sekirei科）","sextuka（sextuka科）","makinosennnixyuu（sennnixyuu科）","sixyouzixyoukoukanntixyou（sixyouzixyoukoukanntixyou科）","akaotaiyoutixyou（taiyoutixyou科）","sizixyuukara（sizixyuukara科）","hasibutogara（sizixyuukara科）","yamagara（sizixyuukara科）","tennninntixyou（tennninntixyou科）","sousitixyou（timedori科）","gabitixyou（timedori科）","tubame（tubame科）","kosiakatubame（tubame科）","sixyoudoutubame（tubame科）","rixyuukixyuutubame（tubame科）","yukihooziro（tumenagahooziro科）","turisugara（turisugara科）","uguisu（uguisu科）","yairotixyou（yairotixyou科）","ooyosikiri（yosikiri科）","datixyou（datixyou科）","hikuidori（hikuidori科）","emixyuー（emixyuー科）","ebosidori（ebosidori科）","yasioumu（oumu科）","okameinnko（oumu科）","hukurououmu（hukurououmu科）","sekiseiinnko（innko科）","honnseiinnko（innko科）","rurikonngouinnko（innko科）","oohuraminngo（huraminngo科）","beniirohuraminngo（huraminngo科）","zixyanomedori（zixyanomedori科）","kaguー（kaguー科）","ketuxaーru（kinubanedori科）","kiーuxi（kiーuxi科）","akagaonezumidori（nezumidori科）","akanogannmodoki（nogannmodoki科）","ahurikaoonogann（nogann科）","zixenntuーpennginn（pennginn科）","aderiーpennginn（pennginn科）","iwatogipennginn（pennginn科）","ousamapennginn（pennginn科）","keーpupennginn（pennginn科）","kouteipennginn（pennginn科）","kogatapennginn（pennginn科）","higepennginn（pennginn科）","hunnborutopennginn（pennginn科）","mazerannpennginn（pennginn科）","rea（rea科）","kannmurisigidatixyou（sigidatixyou科）","tumebakei（tumebakei科）"];
+let birds=[];
+let sub=["キジ","カモ","カイツブリ","ネッタイチョウ","サケイ","ハト","アビ","ミズナギドリ","コウノトリ","カツオドリ","ペリカン","ツル","カッコウ","ヨタカ","アマツバメ","チドリ","タカ","フクロウ","サイチョウ","ブッポウソウ","キツツキ","ハヤブサ","スズメ","ダチョウ","ヒクイドリ","エボシドリ","オウム","フラミンゴ","ジャノメドリ","キヌバネドリ","キーウィ","ネズミドリ","ノガンモドキ","ノガン","ペンギン","レア","シギダチョウ","ツメバケイ"]
+
+function create_photo(){
+    var photo = document.getElementById("lightgallery");
+    let count=2;
+    for (var n=0 ;n<pic_bird.length;n++){
+        u=pic_bird[n].split('（')[0];
+        
+        let bird={name:u,cate:pic_bird[n].split('（')[1].split('）')[0]};
+        birds.push(bird);
+
+        if(n==0){
+            s=pic_bird[n].split('（')[1].split('）')[0];
+            let i=0;
+            var p = document.createElement('p');
+            p.id="i1";
+            var hr =document.createElement('hr');
+            p.innerHTML="<b>"+r2k(s)+"</b>";
+            photo.appendChild(hr);
+            photo.appendChild(p);
+        }
+        if(s!=pic_bird[n].split('（')[1].split('）')[0]){
+            s=pic_bird[n].split('（')[1].split('）')[0]
+            var p = document.createElement('p');
+            var hr =document.createElement('hr');
+            if(r2k(s.split('科')[0])==sub[count-1]){
+                te="i"+count
+                p.id=te;
+                count++;
+                p.innerHTML="<b>"+r2k(s)+"</b>";
+            }
+            else{
+                p.innerHTML=r2k(s);
+            }
+            photo.appendChild(hr);
+            photo.appendChild(p);
+        }
+        var a = document.createElement('a');
+        a.href='bird/'+pic_bird[n]+'.jpg';
+        a.innerHTML='<img src="bird/'+pic_bird[n]+'.jpg" width="240" height="160"/>　'+r2k(pic_bird[n])+'　<br>';
+        photo.appendChild(a);
+    }
+
+    birds.sort(function(a,b){
+        if(a.cate>b.cate){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    })
+    console.log(birds)
+    let bird1=[];
+    let bird2=[];
+    for (var n=0;n<birds.length;n++){
+        if(n==0){
+            s=pic_bird[n].split('（')[1].split('）')[0];
+        }
+        bird2.push(pic_bird[n]);
+        if(s!=pic_bird[n].split('（')[1].split('）')[0] && n!=0){
+            s=pic_bird[n].split('（')[1].split('）')[0]
+            bird2.sort(function(a,b){
+                if(r2k(a)>r2k(b)){
+                    return 1;
+                }
+                else{
+                    return -1;
+                }
+            })
+            bird1.push(bird2);
+            bird2=[]
+        }
+    }
+    text=""
+    for (var n=0;n<bird1.length;n++){
+        text+=bird1[n]+","
+    }
+    t=text.split(",");
+    text=""
+    for (var n=0;n<t.length;n++){
+        text+='"'+t[n]+'",'
+    }
+    
+
+}
+
+//ローマ字をカタカナに変換する
+var roman2hiragana = {
+    'a':'あ', 'i':'い', 'u':'う', 'e':'え', 'o':'お',
+    'ka':'か', 'ki':'き', 'ku':'く', 'ke':'け', 'ko':'こ',
+    'sa':'さ', 'si':'し', 'su':'す', 'se':'せ', 'so':'そ',
+    'ta':'た', 'ti':'ち', 'tu':'つ', 'te':'て', 'to':'と', 'chi':'ち', 'tsu':'つ',
+    'na':'な', 'ni':'に', 'nu':'ぬ', 'ne':'ね', 'no':'の',
+    'ha':'は', 'hi':'ひ', 'hu':'ふ', 'he':'へ', 'ho':'ほ', 'fu':'ふ',
+    'ma':'ま', 'mi':'み', 'mu':'む', 'me':'め', 'mo':'も',
+    'ya':'や', 'yi':'い', 'yu':'ゆ', 'ye':'いぇ', 'yo':'よ',
+    'ra':'ら', 'ri':'り', 'ru':'る', 're':'れ', 'ro':'ろ',
+    'wa':'わ', 'wyi':'ゐ', 'wu':'う', 'wye':'ゑ', 'wo':'を',
+    'nn':'ん',
+    'ga':'が', 'gi':'ぎ', 'gu':'ぐ', 'ge':'げ', 'go':'ご',
+    'za':'ざ', 'zi':'じ', 'zu':'ず', 'ze':'ぜ', 'zo':'ぞ', 'ji':'じ',
+    'da':'だ', 'di':'ぢ', 'du':'づ', 'de':'で', 'do':'ど',
+    'ba':'ば', 'bi':'び', 'bu':'ぶ', 'be':'べ', 'bo':'ぼ',
+    'pa':'ぱ', 'pi':'ぴ', 'pu':'ぷ', 'pe':'ぺ', 'po':'ぽ',
+    'kya':'きゃ', 'kyu':'きゅ', 'kyo':'きょ',
+    'sya':'しゃ', 'syu':'しゅ', 'syo':'しょ',
+    'tya':'ちゃ', 'tyi':'ちぃ', 'tyu':'ちゅ', 'tye':'ちぇ', 'tyo':'ちょ', 'cha':'ちゃ', 'chu':'ちゅ', 'che':'ちぇ', 'cho':'ちょ',
+    'nya':'にゃ', 'nyi':'にぃ', 'nyu':'にゅ', 'nye':'にぇ', 'nyo':'にょ',
+    'hya':'ひゃ', 'hyi':'ひぃ', 'hyu':'ひゅ', 'hye':'ひぇ', 'hyo':'ひょ',
+    'mya':'みゃ', 'myi':'みぃ', 'myu':'みゅ', 'mye':'みぇ', 'myo':'みょ',
+    'rya':'りゃ', 'ryi':'りぃ', 'ryu':'りゅ', 'rye':'りぇ', 'ryo':'りょ',
+    'gya':'ぎゃ', 'gyi':'ぎぃ', 'gyu':'ぎゅ', 'gye':'ぎぇ', 'gyo':'ぎょ',
+    'zya':'じゃ', 'zyi':'じぃ', 'zyu':'じゅ', 'zye':'じぇ', 'zyo':'じょ',
+    'ja':'じゃ', 'ju':'じゅ', 'je':'じぇ', 'jo':'じょ', 'jya':'じゃ', 'jyi':'じぃ', 'jyu':'じゅ', 'jye':'じぇ', 'jyo':'じょ',
+    'dya':'ぢゃ', 'dyi':'ぢぃ', 'dyu':'ぢゅ', 'dye':'ぢぇ', 'dyo':'ぢょ',
+    'bya':'びゃ', 'byi':'びぃ', 'byu':'びゅ', 'bye':'びぇ', 'byo':'びょ',
+    'pya':'ぴゃ', 'pyi':'ぴぃ', 'pyu':'ぴゅ', 'pye':'ぴぇ', 'pyo':'ぴょ',
+    'fa':'ふぁ', 'fi':'ふぃ', 'fe':'ふぇ', 'fo':'ふぉ',
+    'fya':'ふゃ', 'fyu':'ふゅ', 'fyo':'ふょ',
+    'xa':'ぁ', 'xi':'ぃ', 'xu':'ぅ', 'xe':'ぇ', 'xo':'ぉ', 'la':'ぁ', 'li':'ぃ', 'lu':'ぅ', 'le':'ぇ', 'lo':'ぉ',
+    'xya':'ゃ', 'xyu':'ゅ', 'xyo':'ょ',
+    'xtu':'っ', 'xtsu':'っ',
+    'wi':'うぃ', 'we':'うぇ',
+    'va':'ヴぁ', 'vi':'ヴぃ', 'vu':'ヴ', 've':'ヴぇ', 'vo':'ヴぉ'
+};
+  
+function r2k(roman) {
+    var i, iz, match, regex,
+        hiragana = '', table = roman2hiragana;
+  
+    regex = new RegExp((function(table){
+      var key,
+          s = '^(?:';
+  
+      for (key in table) if (table.hasOwnProperty(key)) {
+        s += key + '|';
+      }
+      return s + '(?:n(?![aiueo]|y[aiueo]|$))|' + '([^aiueon])\\1)';
+    })(table));
+    for (i = 0, iz = roman.length; i < iz; ++i) {
+      if (match = roman.slice(i).match(regex)) {
+        if (match[0] === 'n') {
+          hiragana += 'ん';
+        } else if (/^([^n])\1$/.test(match[0])) {
+          hiragana += match[0];
+        } else {
+          hiragana += table[match[0]];
+        }
+        i += match[0].length - 1;
+      } else {
+        hiragana += roman[i];
+      }
+    }
+    return hiraToKana(hiragana);
+}
+
+//ひらがなをカタカナに変換する
+const hiraToKana = text =>{
+    return text.replace( /[\u3042-\u3093]/g, 
+                 m => String.fromCharCode(m.charCodeAt(0) + 96)
+             );
+};
