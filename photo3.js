956bfb59d3ec17aa7736b0fb75fd70985b1e40b1
adazakuraa <99201782+adazakuraa@users.noreply.github.com>
2022-03-01 16:57:05 +0900

    Update photo3.js

--- photo3.js
+++ photo3.js
@@ -1,8 +1,43 @@
-let pic=[]
+let pic=["オオセ","oose","ジンベエザメ","ootennzikuzame","ドチザメ","dotizame","メジロザメ","nemuribuka","アカエイ","akaei","izuhimeei","ウツボ","utubo","torautubo","hanabirautubo","wakautubo","ウミヘビ","moyoumonngaradoosi","ゴンズイ","gonnzui","エソ","minamiakaeso","アシロ","itatiuo","イットウダイ","akamatukasa","ukegutiixtutoudai","seguromatukasa","togariebisu","マツカサウオ","matukasauo","ヤガラ","aoyagara","ヘラヤガラ","herayagara","ヨウジウオ","nokogiriyouzi","ボラ","bora","ダツ","okizayori","メバル","kasago","フサカサゴ","kimiokoze","kirinnmino","nextutaiminokasago","hanaminokasago","minokasago","isokasago","urumakasago","ハオコゼ","haokoze","オニオコゼ","onidarumaokoze","ハタ","kinngixyohanadai","hanagoi","aonomehata","akahata","kannmonnhata","nizihata","barahata","メギス","megisu","kurenainisesuzume","タナバタウオ","simohuritanabatauo","テンジクダイ","kasuriisimoti","kinnsennisimoti","kurohosiisimoti","sukasitennzikudai","suziotennzikudai","miyakoisimoti","rixyuukixyuuyaraiisimoti","アジ","itohikiazi","kasumiazi","simaazi","フエダイ","amimehuedai","ixtutennhuedai","umeiro","barahuedai","huedai","madaratarumi（幼魚）","yosuzihuedai","タカサゴ","ixtusenntakasago","umeiromodoki","クロサギ","kurosagi","イサキ","aziakosixyoudai（若魚）","isaki","korodai","korodai（幼魚）","tixyoutixyoukosixyoudai（幼魚）","hiregurokosixyoudai（幼魚）","musuzikosixyoudai","イトヨリダイ","hutasuzitamagasira","hutasuzitamagasira（幼魚）","yokosimatamagasira","yokosimatamagasira（幼魚）","タイ","kurodai","madai","フエフキダイ","nokogiridai","hamahuehuki","yokosimakurodai","ヒメジ","akahimezi","inndohimezi","oosuzihimezi","okinahimezi","ozisann","hutasuzihimezi","houraihimezi","monntukiakahimezi","ハタンポ","rixyuukixyuuhatannpo","チョウチョウウオ","tunohatatatedai",
+"hatatatedai","minamihatatatedai","oohueyaxtukodai","hueyaxtukodai","akebonotixyoutixyouuo","amitixyoutixyouuo","amimetixyoutixyouuo","ixtutenntixyoutixyouuo","umidukitixyoutixyouuo","kasumitixyoutixyouuo","kagamitixyoutixyouuo","kurakaketixyoutixyouuo","gomatixyoutixyouuo","sitisenntixyoutixyouuo","sirakodai","sudaretixyoutixyouuo","sumitukitonosamadai","segurotixyoutixyouuo","tixyoutixyouuo","tixyouhann","tukitixyoutixyouuo","togetixyoutixyouuo","tonosamadai","nisehuuraitixyoutixyouuo","hanagurotixyoutixyouuo","himehuuraitixyoutixyouuo","huuraitixyoutixyouuo","mikadotixyoutixyouuo","mizoretixyoutixyouuo","yarikatagi","yuuzenn","remonntixyoutixyouuo","キンチャクダイ","akaharayaxtuko","aburayaxtuko","kinntixyakudai","kinntixyakudai（幼魚）","koganeyaxtuko","sazanamiyaxtuko","sazanamiyaxtuko（幼魚）","sitennyaxtuko","somewakeyaxtuko","tatezimakinntixyakudai","tatezimakinntixyakudai（幼魚）","namerayaxtuko","nisikiyaxtuko","ruriyaxtuko","renntennyaxtuko","rokusennyaxtuko","ゴンベ","isogonnbe","sarasagonnbe","himegonnbe","hosigonnbe","minamigonnbe","meganegonnbe","タカノハダイ","takanohadai","migimaki","スズメダイ","kakurekumanomi","kumanomi","sezirokumanomi","touakakumanomi","hanabirakumanomi","hamakumanomi","amamisuzumedai（幼魚）","isigakisuzumedai","itimonnsuzumedai（幼魚）","iwasakisuzumedai","oyabixtutixya","kurakaosuzumedai","kurosuzumedai","kurosuzumedai（幼魚）","kurosorasuzumedai","kuromeganesuzumedai（幼魚）","sikokusuzumedai","sitisennsuzumedai","segurosuzumedai","sedakasuzumedai","sorasuzumedai","danndarasuzumedai","tennzikusuzumedai","debasuzumedai","nagasakisuzumedai","namisuzumedai","nextutaisuzumedai","nezusuzumedai（幼魚）","hakusennsuzumedai","hakusennsuzumedai（幼魚）","hirenagasuzumedai（幼魚）","hutasuzirixyuukixyuusuzumedai（幼魚）","misuzirixyuukixyuusuzumedai","mitubosikurosuzumedai","mitubosikurosuzumedai（幼魚）","miyakokisennsuzumedai（幼魚）","meganesuzumedai（幼魚）","rurisuzumedai","rurihosisuzumedai","rurimeisigakisuzumedai","rokusennsuzumedai","remonnsuzumedai","タカベ","takabe","イシダイ","isigakidai","isidai","イスズミ","isuzumi","カゴカキダイ","kagokakidai","メジナ","mezina","ベラ","akaobiberayg","ira","otomebera","onibera（♀）","ohagurobera",
+"kanokobera（幼魚）","kamasubera","kannmuribera","kannmuribera（幼魚）","gitibera（♀）","gitibera（♂）","kitunebera（幼魚）","kugibera（♀）","kurohutisusukibera","kogasirabera（♀）","koganekixyuusenn","sumitukikaminaribera","sumitukisomewakebera","somewakebera","tokarabera","tuyubera（幼魚）","nisikibera","nisemotinouo","nodogurobera","hakobera","hiregurobera（幼魚）","butisusukibera","butisusukibera（幼魚）","hokutobera","hosisasanohabera","honnsomewakebera","yamabukibera（♀）","yamabukibera（♂）","yannsennnisikibera","meganemotinouo（幼魚）","obitennsumodoki","obitennsumodoki（幼魚）","ブダイ","aobudai","irobudai","irobudai（幼魚）","obibudai","kawaribudai","ダンゴウオ","danngouo","トラギス","ogurotoragisu","イソギンポ","isigakikaeruuo","inaseginnpo","inndokaeruuo","ougonnniziginnpo","tennkurosuziginnpo","nisekurosuziginnpo","hutairokaeruuo","yaeyamaginnpo","ネズッポ","nisikiteguri","ハゼ","akahatihaze","odorihaze","kinubari","kinnsennhaze","クロユリハゼ","kuroyurihaze","hatatatehaze","hanahaze","マンジュウダイ","mikadukitubameuo","アイゴ","amiaigo","sanngoaigo","hihukiaigo","himeaigo","maziriaigo","ツノダシ","tunodasi","ニザダイ","kiirohagi","kurogutiniza","kuromonntuki","kokutennsazanamihagi","gomahagi","gomahagi（幼魚）","sazanamihagi","simahagi","suzikurohagi","tennguhagi","tennguhagimodoki","naganiza","namidakurohagi","nannyouhagi","nizadai","nizihagi","hirenagahagi","hirenagahagi（幼魚）","miyakotennguhagi","meganekurohagi","monntukihagi","カマス","onikamasu","モンガラカワハギ","kumadori","kurakakemonngara","gomamonngara","tasukimonngara","tumaziromonngara","murasamemonngara","monngarakawahagi","カワハギ","amimehagi","umadurahagi","kawahagi","sousihagi","nokogirihagi","tenngukawahagi","ハコフグ","umisuzume","kurohakohugu","simaumisuzume","hakohugu","minamihakohugu（幼魚）","フグ","kitamakura","kusahugu","kokutennhugu","sazanamihugu","ハリセンボン","isigakihugu","harisennbonn","simakinntixyakuhugu"]
+
+let sub=["オオセ","ドチザメ","アカエイ","ウツボ","ゴンズイ","エソ","アシロ","イットウダイ","ヤガラ","ボラ","ダツ","メバル","モンガラカワハギ"]
 
 
+function create_photo(){
+    
 
+    var photo = document.getElementById("lightgallery");
+    let i=0;
+    for (var n=0 ;n<pic.length;n++){
 
+        if(pic[n]==sub[i]){
+            var p = document.createElement('p');
+            p.id="i"+(i+1);
+            console.log(p.id)
+            var hr =document.createElement('hr');
+            p.innerHTML="<b>"+pic[n]+"科</b>";
+            photo.appendChild(hr);
+            photo.appendChild(p);
+            i+=1;
+        }
+        else if(pic[n].match("a") || pic[n].match("i") || pic[n].match("u") || pic[n].match("e") || pic[n].match("o")){
+            var a = document.createElement('a');
+            a.href='images/'+pic[n]+'.jpg';
+            a.setAttribute("data-lightbox", "center");
+            a.innerHTML='<img src="images/'+pic[n]+'.jpg" width="240" height="160"/>　'+r2k(pic[n])+'　<br>';
+            photo.appendChild(a);
+        }
+        else{
+            var p = document.createElement('p');
+            var hr =document.createElement('hr');
+            p.innerHTML=""+pic[n]+"科";
+            photo.appendChild(hr);
+            photo.appendChild(p);
+        }
+    } 
+}
 
 
 
