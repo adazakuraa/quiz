e5153e26bfd615ba95b13abf38c74f433ffca837
adazakuraa <99201782+adazakuraa@users.noreply.github.com>
2023-08-29 23:48:01 +0900

    Update majiaka.js

--- majiaka.js
+++ majiaka.js
@@ -183,8 +183,7 @@ document.addEventListener("keydown", (event) => {
 let id;
 //指定した間隔でappchar関数を呼び出す
 async function int(){
-    let ele = document.getElementById(preint);
-    ele.style.display = 'none';
+    document.getElementById(preint).innerHTML="";
     if((q+1)%10==0){
         if(time!=10){
             time-=10;
